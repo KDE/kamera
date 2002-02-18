070df3b3de9cbfef3a95c6478af00a435ad10b01
Bernhard Rosenkraenzer <bero@arklinux.org>
2002-02-18 17:36:35 +0000

    Fix build with gphoto2 >= 2.0beta4
    svn path=/trunk/kdegraphics/kamera/; revision=137224

--- kcontrol/kamera.h
+++ kcontrol/kamera.h
@@ -6,8 +6,10 @@
 extern "C"
 {
 	#include <gphoto2.h>
-//Defination for this is missing in gphoto2.h
+#ifndef GPHOTO_BETA4
+//Defination for this is missing in gphoto2.h earlier than beta 4
 	int gp_camera_exit (Camera *camera); 
+#endif
 }
 
 class QWidget;
