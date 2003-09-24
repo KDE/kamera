ce3152727962ef6200fd65b05e466ee3e050cbfb
Marcus Meissner <marcus@jet.franken.de>
2003-09-24 21:20:42 +0000

    from 3.1 branch: - off by one error in va_list/vasprintf handling fixed. - if device is in use, try 15 times with 1 second delay to open it.   (so the thumbnail kio job and the download job can use 2 different kamera slaves    without hanging transfers)
    svn path=/trunk/kdegraphics/kamera/; revision=253716

--- kioslave/kamera.cpp
+++ kioslave/kamera.cpp
@@ -138,8 +138,22 @@ void KameraProtocol::autoDetect(void)
 
 // initializes the camera for usage - should be done before operations over the wire
 bool KameraProtocol::openCamera(void) {
-	if (!m_camera)
+	if (!m_camera) {
 		reparseConfiguration();
+	} else {
+		int ret, tries = 15;
+		
+		while (tries--) {
+			ret = gp_camera_init(m_camera, m_context);
+			if (	(ret == GP_ERROR_IO_USB_CLAIM) || 
+				(ret == GP_ERROR_IO_LOCK)) {
+				sleep(1);
+				continue;
+			}
+			if (ret == GP_OK) break;
+			return false;
+		}
+	}
 	return true;
 }
 
@@ -182,8 +196,10 @@ void KameraProtocol::get(const KURL &url)
 		return;
 	}
 
-	if(!openCamera())
+	if(!openCamera()) {
+		error(KIO::ERR_DOES_NOT_EXIST, url.path());
 		return;
+	}
 
 	// fprintf(stderr,"get(%s)\n",url.path().latin1());
 
@@ -349,8 +365,10 @@ void KameraProtocol::statRegular(const KURL &url)
 	UDSEntry entry;
 	int gpr;
 
-	if (openCamera() == false)
+	if (openCamera() == false) {
+		error(KIO::ERR_DOES_NOT_EXIST, url.path());
 		return;
+	}
 
 	// fprintf(stderr,"statRegular(%s)\n",url.path().latin1());
 
@@ -421,8 +439,10 @@ void KameraProtocol::del(const KURL &url, bool isFile)
 {
 	kdDebug(7123) << "KameraProtocol::del(" << url.path() << ")" << endl;
 
-	if(!openCamera())
+	if(!openCamera()) {
+		error(KIO::ERR_CANNOT_DELETE, url.filename());
 		return;
+	}
 	if (!cameraSupportsDel()) {
 		error(KIO::ERR_CANNOT_DELETE, url.filename());
 		return;
@@ -487,8 +507,10 @@ void KameraProtocol::listDir(const KURL &url)
 		return;
 	}
 
-	if (!openCamera())
+	if (!openCamera()) {
+		error(KIO::ERR_COULD_NOT_READ,url.path());
 		return;
+	}
 
 	CameraList *dirList;
 	CameraList *fileList;
@@ -842,7 +864,7 @@ unsigned int frontendProgressStart(
 # elif HAVE___VA_COPY
 	__va_copy(xvalist, args);
 # endif
-	vsnprintf(status, size, format, xvalist);
+	vsnprintf(status, size+1, format, xvalist);
 #else
 	/* We cannot copy the va_list, so make sure we 
 	 * walk it just _once_.
@@ -883,7 +905,7 @@ static void frontendCameraStatus(GPContext * /*context*/, const char *format, va
 # elif HAVE___VA_COPY
 	__va_copy(xvalist, args);
 # endif
-	vsnprintf(status, size, format, xvalist);
+	vsnprintf(status, size+1, format, xvalist);
 #else
 	/* We cannot copy the va_list, so make sure we 
 	 * walk it just _once_.
