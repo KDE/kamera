d09472c2bfca624730f12dc1b8e4088e067064bc
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-06-04 14:39:41 +0200

    Embed JSON metadata in KIOSlaves
    Task: https://phabricator.kde.org/T14518

--- kioslave/kamera.cpp
+++ kioslave/kamera.cpp
@@ -58,6 +58,13 @@ Q_LOGGING_CATEGORY(KAMERA_KIOSLAVE, "kamera.kio")
 
 using namespace KIO;
 
+// Pseudo plugin class to embed meta data
+class KIOPluginForMetaData : public QObject
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID "org.kde.kio.slave.camera" FILE "camera.json")
+};
+
 extern "C"
 {
     Q_DECL_EXPORT int kdemain(int argc, char **argv);
@@ -1199,3 +1206,5 @@ static void frontendCameraStatus(
     object->infoMessage(QString::fromLocal8Bit(status));
 #endif
 }
+
+#include "kamera.moc"
